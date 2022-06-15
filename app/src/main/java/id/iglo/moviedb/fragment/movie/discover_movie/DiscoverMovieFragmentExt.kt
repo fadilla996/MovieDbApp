cc0f885755ee89822449da04ba20716e06490969
ohs00 <fadillasa18@gmail.com>
2022-06-15 17:11:35 +0700

    Commit

--- app/src/main/java/id/iglo/moviedb/fragment/movie/discover_movie/DiscoverMovieFragmentExt.kt
+++ app/src/main/java/id/iglo/moviedb/fragment/movie/discover_movie/DiscoverMovieFragmentExt.kt
@@ -10,29 +10,29 @@ fun DiscoverMovieFragment.observeLiveData() {
     binding.recycler.adapter = adapter.withLoadStateFooter(loadStateAdapter)
     vm.discoverMovie(discoverArgs.genre)
     vm.discoverData.observe(this) {
-        binding.loadingContainer.visibility = android.view.View.GONE
-        binding.recycler.visibility = android.view.View.VISIBLE
-        CoroutineScope(kotlinx.coroutines.Dispatchers.Main).launch {
+        binding.loadingContainer.visibility = View.GONE
+        binding.recycler.visibility = View.VISIBLE
+        CoroutineScope(Dispatchers.Main).launch {
             adapter.submitData(it)
         }
     }
 
     adapter.addLoadStateListener {
         if(it.refresh is LoadState.Error && adapter.itemCount == 0){
-            binding.retryButton.visibility = android.view.View.VISIBLE
-            binding.recycler.visibility = android.view.View.GONE
-            binding.loadingContainer.visibility = android.view.View.GONE
+            binding.retryButton.visibility = View.VISIBLE
+            binding.recycler.visibility = View.GONE
+            binding.loadingContainer.visibility = View.GONE
             binding.retryButton.setOnClickListener{
                 adapter.retry()
             }
         } else if (it.refresh is LoadState.Loading && adapter.itemCount == 0) {
-            binding.retryButton.visibility = android.view.View.GONE
-            binding.recycler.visibility = android.view.View.GONE
-            binding.loadingContainer.visibility = android.view.View.VISIBLE
+            binding.retryButton.visibility = View.GONE
+            binding.recycler.visibility = View.GONE
+            binding.loadingContainer.visibility = View.VISIBLE
         } else if (it.refresh is LoadState.NotLoading) {
-            binding.retryButton.visibility = android.view.View.GONE
-            binding.loadingContainer.visibility = android.view.View.GONE
-            binding.recycler.visibility = android.view.View.VISIBLE
+            binding.retryButton.visibility = View.GONE
+            binding.loadingContainer.visibility = View.GONE
+            binding.recycler.visibility = View.VISIBLE
         }
     }
 }
